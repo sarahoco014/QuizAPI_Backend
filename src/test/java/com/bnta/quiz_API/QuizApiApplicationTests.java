3e9d35a49f0617b0db74863f7c19d2e1550a17da
nimo-mohamed <nimoxmohamed@gmail.com>
2023-08-14 12:55:45 +0100

    project setup

---
+++ src/test/java/com/bnta/quiz_API/QuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.quiz_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
