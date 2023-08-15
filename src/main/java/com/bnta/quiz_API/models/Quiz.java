3a2a6ceeeebc2fbf13f6753e8afe9938c7bf7d97
mikeycb96 <michaelsonbaptist@gmail.com>
2023-08-15 14:46:09 +0100

    added Quiz pojo

---
+++ src/main/java/com/bnta/quiz_API/models/Quiz.java
@@ -0,0 +1,90 @@
+package com.bnta.quiz_API.models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "quizzes")
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToMany(mappedBy = "question_id")//may need to fix
+    private List<Question> questions;
+
+    @Column(name = "current_question_index")
+    private int currentQuestionIndex;
+
+    @Column(name = "current_player")
+    private String currentPlayer;
+
+    @Column
+    private boolean finished;
+
+    @Column
+    private int score;
+
+    public Quiz() {
+        this.id = id;
+        this.questions = List<Question> questions;
+        this.currentQuestionIndex = 0;
+        this.currentPlayer = null;
+        this.finished = false;
+        this.score = 0;
+    }
+
+ //   public Quiz(){
+
+//    } come back to it later
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public int getCurrentQuestionIndex() {
+        return currentQuestionIndex;
+    }
+
+    public void setCurrentQuestionIndex(int currentQuestionIndex) {
+        this.currentQuestionIndex = currentQuestionIndex;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
