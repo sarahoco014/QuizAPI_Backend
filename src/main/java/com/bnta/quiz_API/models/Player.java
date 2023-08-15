4b26d5977c023ad97f2391e080005299745d819d
nimo-mohamed <nimoxmohamed@gmail.com>
2023-08-15 14:24:20 +0100

    added player

---
+++ src/main/java/com/bnta/quiz_API/models/Player.java
@@ -0,0 +1,52 @@
+package com.bnta.quiz_API.models;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "players")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @OneToMany(mappedBy = "currentPlayer")
+    private List<Quiz> quizzes;
+
+    public Player(String name) {
+        this.id = id;
+        this.name = name;
+        this.quizzes = new ArrayList<Quiz>();
+    }
+
+    public Player() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Quiz> getQuizzes() {
+        return quizzes;
+    }
+
+    public void setQuizzes(List<Quiz> quizzes) {
+        this.quizzes = quizzes;
+    }
+}
