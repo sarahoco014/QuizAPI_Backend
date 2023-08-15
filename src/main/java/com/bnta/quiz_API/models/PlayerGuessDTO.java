594cc0fa6e0357430631ba45d92a6aca462d0c53
nimo-mohamed <nimoxmohamed@gmail.com>
2023-08-15 15:56:56 +0100

    playerGuessDTO addded'

---
+++ src/main/java/com/bnta/quiz_API/models/PlayerGuessDTO.java
@@ -0,0 +1,19 @@
+package com.bnta.quiz_API.models;
+
+public class PlayerGuessDTO {
+    private String guess;
+
+    public PlayerGuessDTO(String guess){
+        this.guess = guess;
+    }
+    public PlayerGuessDTO() {
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+}
