b364e4ee6e47e592339bd2a4534a10b0cb94ea8a
mikeycb96 <michaelsonbaptist@gmail.com>
2023-08-15 16:03:55 +0100

    added QuestionRepo

--- src/main/java/com/bnta/quiz_API/repositories/PlayerRepository.java
+++ src/main/java/com/bnta/quiz_API/repositories/PlayerRepository.java
@@ -2,6 +2,8 @@ package com.bnta.quiz_API.repositories;
 
 import com.bnta.quiz_API.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository <Player, Integer> {
 }
--- src/main/java/com/bnta/quiz_API/repositories/QuestionRepository.java
+++ src/main/java/com/bnta/quiz_API/repositories/QuestionRepository.java
@@ -1,9 +1,9 @@
 package com.bnta.quiz_API.repositories;
 
-import com.bnta.quiz_API.models.Quiz;
+import com.bnta.quiz_API.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuizRepository extends JpaRepository <Quiz, Integer> {
+public interface QuestionRepository extends JpaRepository <Question, Integer> {
 }
