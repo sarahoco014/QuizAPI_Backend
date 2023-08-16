33c735534446b74f94f334d8b0cd8bcc5b880c71
hadams0 <adamshannah1@icloud.com>
2023-08-16 12:52:24 +0100

    added questions and players to data laoder

--- src/main/java/com/bnta/quiz_API/components/DataLoader.java
+++ src/main/java/com/bnta/quiz_API/components/DataLoader.java
@@ -3,6 +3,7 @@ package com.bnta.quiz_API.components;
 import com.bnta.quiz_API.models.Answer;
 import com.bnta.quiz_API.models.Player;
 import com.bnta.quiz_API.models.Question;
+import com.bnta.quiz_API.models.Quiz;
 import com.bnta.quiz_API.repositories.AnswerRepository;
 import com.bnta.quiz_API.repositories.PlayerRepository;
 import com.bnta.quiz_API.repositories.QuestionRepository;
@@ -36,6 +37,9 @@ public class DataLoader implements ApplicationRunner {
         Question question1 = new Question("What is the world's longest river?");
         questionRepository.save(question1);
 
+        Question question2 = new Question("What is the highest mountain in the world?");
+        questionRepository.save(question2);
+
         // Create answers
 
         Answer answer1 = new Answer("Nile", true, question1);
@@ -45,11 +49,32 @@ public class DataLoader implements ApplicationRunner {
         answerRepository.save(answer2);
         answerRepository.save(answer3);
 
+        Answer answer4 = new Answer("Kilimanjaro", false, question2);
+        Answer answer5 = new Answer("Ben Nevis", false, question2);
+        Answer answer6 = new Answer("Mount Everest", true, question2);
+        answerRepository.save(answer4);
+        answerRepository.save(answer5);
+        answerRepository.save(answer6);
+
+
         // Create a player
 
-        // Add player to quiz
+        Player player1 = new Player("Jon");
+        Player player2 = new Player("Jenifer");
+        playerRepository.save(player1);
+        playerRepository.save(player2);
 
+        // Create quiz
+        // Add player to quiz
         // Add question to quiz
+
+        Quiz quiz1 = new Quiz();
+        quiz1.getQuestions().add(question1);
+        quiz1.getQuestions().add(question2);
+        quiz1.setCurrentPlayer(player1);
+        quizRepository.save(quiz1);
+
+
 
     }
 }
